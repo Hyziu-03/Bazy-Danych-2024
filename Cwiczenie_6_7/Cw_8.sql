300db940949769299fbe1e2af5b7334ca727ed58
Szymon Hyziak <szymon.hyziak@protonmail.com>
2024-12-03 14:02:43 +0100

    Przywrócenie bazy i załadowanie wysokości

---
+++ Cwiczenie_6_7/Cw_8.sql
@@ -0,0 +1,13 @@
+ALTER SCHEMA schema_name RENAME TO hyziak;
+
+CREATE EXTENSION postgis_raster;
+
+CREATE TABLE hyziak.intersects AS
+SELECT a.rast, b.municipality
+FROM rasters.dem AS a, vectors.porto_parishes AS b
+WHERE ST_Intersects(a.rast, b.geom) AND b.municipality ilike 'porto';
+
+CREATE TABLE hyziak.clip AS
+SELECT ST_Clip(a.rast, b.geom, true), b.municipality
+FROM rasters.dem AS a, vectors.porto_parishes AS b
+WHERE ST_Intersects(a.rast, b.geom) AND b.municipality like 'PORTO';
